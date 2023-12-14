55b40372d2b3326e2e140b3736a1c2e5d32cce84
soumya-99 <soumyadeepmondal1234@gmail.com>
2023-12-14 16:26:35 +0530

    api integrated - not fully

---
+++ lib/api_key.dart
@@ -0,0 +1,1 @@
+const openWeatherAPIKey = "1ae19e5defc89345c42a1f4f9ed12c06";
--- lib/weather_material_screen.dart
+++ lib/weather_material_screen.dart
@@ -1,11 +1,51 @@
+import 'dart:convert';
 import 'dart:ui';
+import 'package:http/http.dart' as http;
 
 import 'package:flutter/material.dart';
 import 'package:weather_app/additional_info_item.dart';
+import 'package:weather_app/api_key.dart';
 import 'package:weather_app/hourly_forecast_item.dart';
 
-class WeatherScreen extends StatelessWidget {
+class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
+
+  @override
+  State<WeatherScreen> createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  double temp = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentWeather();
+  }
+
+  Future getCurrentWeather() async {
+    String cityName = "London";
+
+    try {
+      final res = await http.get(
+        Uri.parse(
+          "https://api.openweathermap.org/data/2.5/forecast?q=$cityName&APPID=$openWeatherAPIKey",
+        ),
+      );
+
+      final data = jsonDecode(res.body);
+
+      if (data['cod'] != '200') {
+        throw "An unexpeced error occurred";
+      }
+
+      setState(() {
+        temp = data['list'][0]['main']['temp'];
+      });
+    } catch (e) {
+      throw e.toString();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,126 +67,128 @@ class WeatherScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // main card
-            SizedBox(
-              width: double.infinity,
-              child: Card(
-                elevation: 10,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(16),
-                  child: BackdropFilter(
-                    filter: ImageFilter.blur(
-                      sigmaX: 5,
-                      sigmaY: 5,
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            "300 K",
-                            style: TextStyle(
-                              fontSize: 32,
-                              fontWeight: FontWeight.bold,
+      body: temp == 0
+          ? const CircularProgressIndicator()
+          : Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // main card
+                  SizedBox(
+                    width: double.infinity,
+                    child: Card(
+                      elevation: 10,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: BackdropFilter(
+                          filter: ImageFilter.blur(
+                            sigmaX: 5,
+                            sigmaY: 5,
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              children: [
+                                Text(
+                                  "$temp K",
+                                  style: const TextStyle(
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 16),
+                                const Icon(
+                                  Icons.cloud,
+                                  size: 50,
+                                ),
+                                const SizedBox(height: 16),
+                                const Text(
+                                  "Rain",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                          SizedBox(height: 16),
-                          Icon(
-                            Icons.cloud,
-                            size: 50,
-                          ),
-                          SizedBox(height: 16),
-                          Text(
-                            "Rain",
-                            style: TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                        ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            // weather forecast cards
-            const Text(
-              "Weather Forecast",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 16),
-            const SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  HourlyForecastItem(
-                    time: "00:00",
-                    icon: Icons.cloud,
-                    temparature: "301.22",
+                  const SizedBox(height: 20),
+                  // weather forecast cards
+                  const Text(
+                    "Weather Forecast",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                  HourlyForecastItem(
-                    time: "03:00",
-                    icon: Icons.sunny,
-                    temparature: "301.22",
+                  const SizedBox(height: 16),
+                  const SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        HourlyForecastItem(
+                          time: "00:00",
+                          icon: Icons.cloud,
+                          temparature: "301.22",
+                        ),
+                        HourlyForecastItem(
+                          time: "03:00",
+                          icon: Icons.sunny,
+                          temparature: "301.22",
+                        ),
+                        HourlyForecastItem(
+                          time: "06:00",
+                          icon: Icons.sunny,
+                          temparature: "302.12",
+                        ),
+                        HourlyForecastItem(
+                          time: "09:00",
+                          icon: Icons.cloud,
+                          temparature: "300.52",
+                        ),
+                      ],
+                    ),
                   ),
-                  HourlyForecastItem(
-                    time: "06:00",
-                    icon: Icons.sunny,
-                    temparature: "302.12",
+                  // additional info
+                  const SizedBox(height: 16),
+                  const Text(
+                    "Additional Info",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                  HourlyForecastItem(
-                    time: "09:00",
-                    icon: Icons.cloud,
-                    temparature: "300.52",
-                  ),
+                  const SizedBox(height: 16),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      AdditionalInfoItem(
+                        icon: Icons.water_drop,
+                        label: "Humidity",
+                        value: "91",
+                      ),
+                      AdditionalInfoItem(
+                        icon: Icons.air,
+                        label: "Wind Speed",
+                        value: "7.5",
+                      ),
+                      AdditionalInfoItem(
+                        icon: Icons.beach_access,
+                        label: "Pressure",
+                        value: "1000",
+                      ),
+                    ],
+                  )
                 ],
               ),
             ),
-            // additional info
-            const SizedBox(height: 16),
-            const Text(
-              "Additional Info",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 16),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                AdditionalInfoItem(
-                  icon: Icons.water_drop,
-                  label: "Humidity",
-                  value: "91",
-                ),
-                AdditionalInfoItem(
-                  icon: Icons.air,
-                  label: "Wind Speed",
-                  value: "7.5",
-                ),
-                AdditionalInfoItem(
-                  icon: Icons.beach_access,
-                  label: "Pressure",
-                  value: "1000",
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
     );
   }
 }
